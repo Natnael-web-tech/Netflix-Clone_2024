b2a99a8344949365e4749e6ec90be1fd40ed1c19
Natnael-web-tech <gelalchanatnael@gmail.com>
2025-02-10 14:16:29 -0500

    Raw

--- netflix-clone/src/Components/Banner/Banner.jsx
+++ netflix-clone/src/Components/Banner/Banner.jsx
@@ -1,9 +1,13 @@
 import React, { useEffect, useState } from "react";
-import axios from "../../Utils/axios"; 
-import requests from "../../Utils/requests"; 
-import './Banner.css'
+import axios from "../../Utils/axios";
+import requests from "../../Utils/requests";
+import "./Banner.css";
+import movieTrailer from "movie-trailer";
+import YouTube from "react-youtube";
+
 const Banner = () => {
   const [movie, setMovie] = useState(null);
+  const [trailerUrl, setTrailerUrl] = useState("");
 
   useEffect(() => {
     async function fetchData() {
@@ -14,31 +18,65 @@ const Banner = () => {
     fetchData();
   }, []);
 
-function truncate(str, n) {
-  return str?.length > n ? str.substr(0, n-1) + '...' :str;
-}
+  function truncate(str, n) {
+    return str?.length > n ? str.substr(0, n - 1) + "..." : str;
+  }
+
+  const handleClick = (movie) => {
+    if (trailerUrl) {
+      setTrailerUrl("");
+    } else {
+      movieTrailer(movie?.title || movie?.name || movie?.original_name)
+        .then((url) => {
+          const urlParams = new URLSearchParams(new URL(url).search);
+          setTrailerUrl(urlParams.get("v"));
+        })
+        .catch((error) => console.log(error));
+    }
+  };
+
+  const opts = {
+    height: "390",
+    width: "100%",
+    playerVars: { autoplay: 1 },
+  };
 
   return (
-    <div
-      className="banner"
-      style={{
-        backgroundSize: "cover",
-        backgroundImage: `url("https://image.tmdb.org/t/p/original${movie?.backdrop_path}")`,
-        backgroundPosition: "center center",
-      }}
-    >
-      <div className="banner_contents">
-        <h1 className="banner_title">{ movie?.title || movie?.name || movie?.original_name}</h1>
-        <div className="banner_buttons">
-<button className="banner_button play">Play</button>
-<button className="banner_button">My List</button>
+    <>
+      <div
+        className="banner"
+        style={{
+          backgroundSize: "cover",
+          backgroundImage: `url("https://image.tmdb.org/t/p/original${movie?.backdrop_path}")`,
+          backgroundPosition: "center center",
+        }}
+      >
+        <div className="banner_contents">
+          <h1 className="banner_title">
+            {movie?.title || movie?.name || movie?.original_name}
+          </h1>
+          <div className="banner_buttons">
+            <button
+              className="banner_button play"
+              onClick={() => handleClick(movie)}
+            >
+              Play
+            </button>
+            <button className="banner_button">My List</button>
+          </div>
+          <h1 className="banner_description">
+            {truncate(movie?.overview, 150)}
+          </h1>
         </div>
-        <h1 className="banner_description">{truncate(movie?.overview, 150)}</h1>
+        <div className="banner_fadeBottom"></div>
       </div>
-      <div className="banner_fadeBottom">
 
-      </div>
-    </div>
+      {trailerUrl && (
+        <div className="video_container">
+          <YouTube videoId={trailerUrl} opts={opts} />
+        </div>
+      )}
+    </>
   );
 };
 
